5213b75fbe527d48395d6910ec4e7746559dc97e
Aarón I. Vélez Ramírez <aaron.velez@comunidad.unam.mx>
2022-03-10 21:41:20 -0600

    Main Arduino proyect created

---
+++ Main/Main.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Main.ino
+ Created:	3/10/2022 9:39:46 PM
+ Author:	aivel
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
